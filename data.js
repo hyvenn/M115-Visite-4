0cd917b8db24694a078fe05de31265fded79cd40
hyvenn <56298319+hyvenn@users.noreply.github.com>
2021-06-14 15:05:59 +0200

    visite 4

---
+++ data.js
@@ -0,0 +1,761 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r7-haut-escalier-sol-bton",
+      "name": "R+7 haut escalier sol béton",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7322509334095013,
+          "pitch": 0.05403284681540832,
+          "rotation": 0,
+          "target": "12-r7-cuisine"
+        },
+        {
+          "yaw": -0.929831480574105,
+          "pitch": 0.7972510762862335,
+          "rotation": 0,
+          "target": "10-r6-bureaux"
+        },
+        {
+          "yaw": -2.2607589158242725,
+          "pitch": 0.07756112048907582,
+          "rotation": 0,
+          "target": "2-r7-terrasse"
+        },
+        {
+          "yaw": -3.0771134661721735,
+          "pitch": 1.4967820912592344,
+          "rotation": 0,
+          "target": "1-r7-haut-escalier-sol-moquette"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-r7-haut-escalier-sol-moquette",
+      "name": "R+7 haut escalier sol moquette",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7815792371896606,
+          "pitch": 1.429704631697815,
+          "rotation": 0,
+          "target": "0-r7-haut-escalier-sol-bton"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-r7-terrasse",
+      "name": "R+7 terrasse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7093645336489267,
+          "pitch": 0.14308656226578265,
+          "rotation": 10.995574287564278,
+          "target": "0-r7-haut-escalier-sol-bton"
+        },
+        {
+          "yaw": 1.0410610308231618,
+          "pitch": -0.4448681190768049,
+          "rotation": 0.7853981633974483,
+          "target": "3-comble-accs-piscine"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-comble-accs-piscine",
+      "name": "Comble accés piscine",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5745130004045649,
+          "pitch": 0.687481648526445,
+          "rotation": 4.71238898038469,
+          "target": "2-r7-terrasse"
+        },
+        {
+          "yaw": 0.0073295705130806965,
+          "pitch": -0.2950254837415578,
+          "rotation": 0,
+          "target": "4-piscine-sur-le-toit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-piscine-sur-le-toit",
+      "name": "Piscine sur le toit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09161049204521277,
+          "pitch": 0.6952002039159115,
+          "rotation": 0,
+          "target": "3-comble-accs-piscine"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-r5-accueil",
+      "name": "R+5 accueil",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": 0.6273665329441442,
+        "pitch": 0.04428108642899531,
+        "fov": 1.41569238083293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5301783898753918,
+          "pitch": -0.3788628075326912,
+          "rotation": 0,
+          "target": "10-r6-bureaux"
+        },
+        {
+          "yaw": 0.9804803457883313,
+          "pitch": -0.032054577974840015,
+          "rotation": 0,
+          "target": "6-r5-bureaux-nord"
+        },
+        {
+          "yaw": -0.8470793231391482,
+          "pitch": 0.008334607982792619,
+          "rotation": 1.5707963267948966,
+          "target": "7-r5-bureaux-sud"
+        },
+        {
+          "yaw": 1.7702962370968187,
+          "pitch": -0.015397617093984906,
+          "rotation": 0,
+          "target": "8-r5-repro"
+        },
+        {
+          "yaw": 0.4854223806484512,
+          "pitch": 1.5343295497357268,
+          "rotation": 0,
+          "target": "14-r5-accueil-sol-bton"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-r5-bureaux-nord",
+      "name": "R+5 bureaux nord",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.24647329020148945,
+          "pitch": -0.10393076640488275,
+          "rotation": 8.63937979737193,
+          "target": "8-r5-repro"
+        },
+        {
+          "yaw": 1.2538281559802034,
+          "pitch": -0.052395205886099916,
+          "rotation": 3.141592653589793,
+          "target": "7-r5-bureaux-sud"
+        },
+        {
+          "yaw": 0.8294600104164633,
+          "pitch": -0.047026738760246545,
+          "rotation": 3.9269908169872414,
+          "target": "5-r5-accueil"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-r5-bureaux-sud",
+      "name": "R+5 bureaux sud",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.307199275875913,
+          "pitch": -0.052410303424636595,
+          "rotation": 3.141592653589793,
+          "target": "6-r5-bureaux-nord"
+        },
+        {
+          "yaw": 1.6160413815079924,
+          "pitch": -0.11323241459879796,
+          "rotation": 3.9269908169872414,
+          "target": "5-r5-accueil"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-r5-repro",
+      "name": "R+5 repro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.34946430780538407,
+          "pitch": -0.036077737110614905,
+          "rotation": 3.141592653589793,
+          "target": "5-r5-accueil"
+        },
+        {
+          "yaw": 1.138070593748667,
+          "pitch": -0.0650683969818413,
+          "rotation": 3.141592653589793,
+          "target": "6-r5-bureaux-nord"
+        },
+        {
+          "yaw": 0.16363711767177058,
+          "pitch": -0.05682769725937575,
+          "rotation": 10.210176124166829,
+          "target": "7-r5-bureaux-sud"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-r6-bureau-serge",
+      "name": "R+6 bureau Serge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.839776491622298,
+          "pitch": 0.09442786988054586,
+          "rotation": 5.497787143782138,
+          "target": "10-r6-bureaux"
+        },
+        {
+          "yaw": 1.0854741265295331,
+          "pitch": -0.02241098218749471,
+          "rotation": 3.141592653589793,
+          "target": "11-r6-runion"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-r6-bureaux",
+      "name": "R+6 bureaux",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7081395229207867,
+          "pitch": -0.0033953737950440654,
+          "rotation": 0.7853981633974483,
+          "target": "9-r6-bureau-serge"
+        },
+        {
+          "yaw": 1.0988267676074148,
+          "pitch": -0.07009171457832153,
+          "rotation": 5.497787143782138,
+          "target": "0-r7-haut-escalier-sol-bton"
+        },
+        {
+          "yaw": 0.9358902672981841,
+          "pitch": 0.2496707098143478,
+          "rotation": 2.356194490192345,
+          "target": "5-r5-accueil"
+        },
+        {
+          "yaw": 1.6802241328231329,
+          "pitch": 0.06211107158190643,
+          "rotation": 0,
+          "target": "11-r6-runion"
+        },
+        {
+          "yaw": 0.973174624803903,
+          "pitch": 1.5707963267948966,
+          "rotation": 0,
+          "target": "13-r6-bureaux-sol-bton"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-r6-runion",
+      "name": "R+6 réunion",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3805335049145704,
+          "pitch": -0.02541527657754017,
+          "rotation": 3.141592653589793,
+          "target": "9-r6-bureau-serge"
+        },
+        {
+          "yaw": -0.7406104250672954,
+          "pitch": 0.05265247324721223,
+          "rotation": 0,
+          "target": "10-r6-bureaux"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-r7-cuisine",
+      "name": "R+7 cuisine",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3639932588019548,
+          "pitch": 0.013561022363077768,
+          "rotation": 3.141592653589793,
+          "target": "0-r7-haut-escalier-sol-bton"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-r6-bureaux-sol-bton",
+      "name": "R+6 bureaux sol béton",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7512250965857028,
+          "pitch": 1.5707963267948966,
+          "rotation": 0,
+          "target": "10-r6-bureaux"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-r5-accueil-sol-bton",
+      "name": "R+5 accueil sol béton",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5271382907651301,
+          "pitch": 1.5707963267948966,
+          "rotation": 0,
+          "target": "5-r5-accueil"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "M115 Visite 4",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
